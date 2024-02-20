e07ab68928bcf1d7a4d127f02b5567f2f3325166
root <hugh.fagan123@gmail.com>
2024-02-20 03:42:36 +0000

    Fixing program

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
