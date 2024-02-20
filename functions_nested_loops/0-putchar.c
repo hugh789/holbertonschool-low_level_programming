76448107cff3eeb9920e6ef4b6be03b1aac15b6d
root <hugh.fagan123@gmail.com>
2024-02-20 03:47:10 +0000

    Fixed to betty style documentation

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
-#include <stdio.h>  // Include the <stdio.h> header file
+#include <stdio.h>
 
 /**
- * main - Print _putchar
- * Return: Always 0
+ * main - Entry point of the program
+ *
+ * Description: Prints each character of "_putchar" followed by a newline
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    putchar('_');  // Use putchar instead of _putchar
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return (0);
+    putchar('_'); /* Print the character '_' */
+    putchar('p'); /* Print the character 'p' */
+    putchar('u'); /* Print the character 'u' */
+    putchar('t'); /* Print the character 't' */
+    putchar('c'); /* Print the character 'c' */
+    putchar('h'); /* Print the character 'h' */
+    putchar('a'); /* Print the character 'a' */
+    putchar('r'); /* Print the character 'r' */
+    putchar('\n'); /* Print a newline character */
+    return (0); /* Return 0 to indicate success */
 }
