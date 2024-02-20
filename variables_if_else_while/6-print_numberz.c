a20f87956054012359300e874cce1c64f6ef3984
root <hugh.fagan123@gmail.com>
2024-02-20 00:39:06 +0000

    Fixing volume of putchar

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints numbers from 0 to 9 separated by commas and spaces
- *
- * Return: Always 0 (Success)
- */
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-    int x;
+	int n;
 
-    for (x = 0; x < 10; x++)
-    {
-        putchar(x + '0');
-        if (x < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-        else
-        {
-            putchar('\n');
-        }
-    }
-    return (0);
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
