5a798b9b69ef4fe498ee446cfc6c5e5de7f2e4a0
root <hugh.fagan123@gmail.com>
2024-02-20 04:08:22 +0000

    Removed spaces

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-    _putchar('_'); /* Print the character '_' */
-    _putchar('p'); /* Print the character 'p' */
-    _putchar('u'); /* Print the character 'u' */
-    _putchar('t'); /* Print the character 't' */
-    _putchar('c'); /* Print the character 'c' */
-    _putchar('h'); /* Print the character 'h' */
-    _putchar('a'); /* Print the character 'a' */
-    _putchar('r'); /* Print the character 'r' */
-    _putchar('\n'); /* Print a newline character */
-    return (0); /* Return 0 to indicate success */
+  _putchar('_'); /* Print the character '_' */
+  _putchar('p'); /* Print the character 'p' */
+  _putchar('u'); /* Print the character 'u' */
+  _putchar('t'); /* Print the character 't' */
+  _putchar('c'); /* Print the character 'c' */
+  _putchar('h'); /* Print the character 'h' */
+  _putchar('a'); /* Print the character 'a' */
+  _putchar('r'); /* Print the character 'r' */
+  _putchar('\n'); /* Print a newline character */
+  return (0); /* Return 0 to indicate success */
 }
