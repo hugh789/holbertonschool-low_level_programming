1e39604bc10bf51d87dcd7246fdf6cb2d6473598
root <hugh.fagan123@gmail.com>
2024-02-27 02:41:27 +0000

    Adding a function that prints a square, followed by a new line and editing main.h with a new prototype

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square made of '#' characters
+ * @size: Size of the square
+ *
+ * Description: This function prints a square made of '#' characters.
+ *              The square has a width and height equal to 'size'.
+ *              If 'size' is less than or equal to 0, it prints only a newline.
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
