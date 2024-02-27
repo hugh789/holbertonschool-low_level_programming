93b20f6f915b4f509b782fc8a86a25e66af2c51c
root <hugh.fagan123@gmail.com>
2024-02-27 02:54:34 +0000

    fix betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  *
- * Description: Prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
- *              multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".
- *
+ * Description: prints the numbers 1 - 100 w/
+ * Fizz for multiples of 3, Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
  * Return: Always 0 (Success)
  */
 int main(void)
