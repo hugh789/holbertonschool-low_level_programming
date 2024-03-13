aa3e05eb84586e9fb70a0241bbe284fdc7b57444
root <hugh.fagan123@gmail.com>
2024-03-13 02:37:04 +0000

    syntax error fix did not save in emacs:

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 /**
  * main - prints all arguments it receives.
- * @argc: number of comand line arguments. 
- *@argv: array that contains the program command line arguments. 
- *Return: 0 - success
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-    int i;
-    
-    
-    for(i = 0; i < argc; i++)
-    
-        printf("%s\n",*argv[i]);
-    return (0);
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
 }
