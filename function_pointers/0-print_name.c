564c8339286bc0b6e55b159e080bd1262192c05a
root <hugh.fagan123@gmail.com>
2024-03-19 00:03:14 +0000

    Code change

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include <function_pointers.h> 
-
+#include "function_pointers.h"
 
 /**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
+* print_name - prints a name.
+* @name: name of the person.
+* @f: pointer to function.
+* Return: Nothing.
+*/
 
-int main(void)
+void print_name(char *name, void (*f)(char *))
 {
-    char *p = "Bob";
-    printf(p);
-    return 0;
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	f(name);
 }
