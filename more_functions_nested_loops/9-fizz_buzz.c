c14f492aa643d0c08af7c2e61d16d70932d079e9
root <hugh.fagan123@gmail.com>
2024-04-02 02:48:42 +0000

    fixing betty code

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,15 +3,19 @@
 /**
  * main - Entry point
  *
- * Description: prints the numbers 1 - 100 w/
+ * Description: prints the numbers 1 - 100 with
  * Fizz for multiples of 3, Buzz for multiples of 5,
  * and FizzBuzz for multiples of both
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i = 0; char B[88] ;
+    int i = 0;
+    char B[88];
+
     while (i++ < 100)
-    !sprintf (B, "%s%s", i%3 ? "":"Fizz", i%5 ? "":"Buzz")
-    ? sprintf(B, "%d", i):0, printf (", %s", B);
+        !sprintf(B, "%s%s", i % 3 ? "" : "Fizz", i % 5 ? "" : "Buzz")
+            ? sprintf(B, "%d", i) : 0, printf(", %s", B);
+
+    return (0);
 }
