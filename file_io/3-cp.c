3739e95660f2672803aecb2f45181a7c0251fcd6
root <hugh.fagan123@gmail.com>
2024-04-10 02:49:10 +0000

    3-cp second attempt

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,69 +1,94 @@
 #include "main.h"
-#define BUF_SIZE 1024
 
 /**
- *__exit - function that prints error messages and exits with exit value
- *@error: num is either exit value or file descriptor
- *@s: str is a name, either of the two filenames
- *@fd: file descriptor
- *Return: 0 on success
- **/
+ * open_source_file - opens the source file
+ * @filename: the name of the file to open
+ *
+ * Return: the file descriptor of the opened file
+ */
 
-int __exit(int error, char *s, int fd)
+int open_source_file(const char *filename)
 {
-	switch (error)
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-	case 97:
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(error);
-	case 98:
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-		exit(error);
-	case 99:
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-		exit(error);
-	case 100:
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(error);
-	default:
-		return (0);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
 	}
+	return (fd);
 }
 
 /**
- *main - copies the content of a file to another file
- *@argc: number of arguments passed to the program
- *@argv: array of arguments passed to the program
- *Return: 0 on success, or an error code on failure
+ * create_dest_file - creates the destination file
+ * @filename: the name of the file to be created
+ *
+ * Return: the file descriptor of the created file
+ */
+
+int create_dest_file(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				filename);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
+ * main - Entry point of thr program
+ *
+ * @argc: Number of arguments
+ * @argv: Command-line arguments
+ *
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
+	int fd1, fd2;
+	ssize_t bytesread, byteswritten;
 	char buffer[1024];
 
 	if (argc != 3)
-		__exit(97, NULL, 0);
-
-	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd_to == -1)
-		__exit(99, argv[2], 0);
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		__exit(98, argv[1], 0);
-
-	while ((bytes_read = read(fd_from, buffer, 1024)) != 0)
 	{
-		if (bytes_read == -1)
-			__exit(98, argv[1], 0);
-
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-			__exit(99, argv[2], 0);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
+	fd1 = open_source_file(argv[1]);
+	fd2 = create_dest_file(argv[2]);
 
-	close(fd_to) == -1 ? (__exit(100, NULL, fd_to)) : close(fd_to);
-	close(fd_from) == -1 ? (__exit(100, NULL, fd_from)) : close(fd_from);
+	while ((bytesread = read(fd1, buffer, 1024)) > 0)
+	{
+		byteswritten = write(fd2, buffer, bytesread);
+		if (byteswritten != bytesread)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd1);
+			close(fd2);
+			exit(99);
+		}
+	}
+	if (bytesread == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
 	return (0);
 }
